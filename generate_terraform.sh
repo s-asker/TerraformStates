5b3302ecc1a5f05754a55cff7d303d8f3297cad6
s-asker <sherifasker2@gmail.com>
2024-10-08 17:34:12 +0300

    Version 2

--- generate_terraform.sh
+++ generate_terraform.sh
@@ -40,7 +40,7 @@ fi
 VPC_CIDR=$(aws ec2 describe-vpcs --vpc-ids $VPC_ID --query 'Vpcs[0].CidrBlock' --output text --region $REGION)
 
 # Generate Terraform configuration for VPC
-generate_tf "vpc" "s/{{RESOURCE_ID}}/$VPC_ID/g; s/{{CIDR_BLOCK}}/$VPC_CIDR/g" "templates/vpc_template.tf.j2" "aws_vpc_$VPC_ID.tf"
+generate_tf "vpc" "s|{{RESOURCE_ID}}|$VPC_ID|g; s|{{CIDR_BLOCK}}|$VPC_CIDR|g" "templates/vpc_template.tf.j2" "aws_vpc_$VPC_ID.tf"
 
 # Import the VPC into Terraform
 import_resource "aws_vpc.example" "$VPC_ID"
@@ -48,21 +48,21 @@ import_resource "aws_vpc.example" "$VPC_ID"
 # Get Subnets in the VPC and generate/import Terraform files for each
 SUBNETS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --query 'Subnets[*].[SubnetId,CidrBlock,AvailabilityZone]' --output text --region $REGION)
 while read -r SUBNET_ID SUBNET_CIDR AZ; do
-    generate_tf "subnet" "s/{{RESOURCE_ID}}/$SUBNET_ID/g; s/{{CIDR_BLOCK}}/$SUBNET_CIDR/g; s/{{AVAILABILITY_ZONE}}/$AZ/g" "templates/subnet_template.tf.j2" "aws_subnet_$SUBNET_ID.tf"
+    generate_tf "subnet" "s|{{RESOURCE_ID}}|$SUBNET_ID|g; s|{{CIDR_BLOCK}}|$SUBNET_CIDR|g; s|{{AVAILABILITY_ZONE}}|$AZ|g" "templates/subnet_template.tf.j2" "aws_subnet_$SUBNET_ID.tf"
     import_resource "aws_subnet.example" "$SUBNET_ID"
 done <<< "$SUBNETS"
 
 # Get Security Groups in the VPC and generate/import Terraform files for each
 SGS=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$VPC_ID" --query 'SecurityGroups[*].[GroupId,GroupName,Description]' --output text --region $REGION)
 while read -r SG_ID SG_NAME SG_DESC; do
-    generate_tf "security_group" "s/{{RESOURCE_ID}}/$SG_ID/g; s/{{GROUP_NAME}}/$SG_NAME/g; s/{{DESCRIPTION}}/$SG_DESC/g" "templates/security_group_template.tf.j2" "aws_security_group_$SG_ID.tf"
+    generate_tf "security_group" "s|{{RESOURCE_ID}}|$SG_ID|g; s|{{GROUP_NAME}}|$SG_NAME|g; s|{{DESCRIPTION}}|$SG_DESC|g" "templates/security_group_template.tf.j2" "aws_security_group_$SG_ID.tf"
     import_resource "aws_security_group.example" "$SG_ID"
 done <<< "$SGS"
 
 # Get EC2 Instances in the VPC and generate/import Terraform files for each
 INSTANCES=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress]' --output text --region $REGION)
 while read -r INSTANCE_ID INSTANCE_TYPE PRIVATE_IP; do
-    generate_tf "instance" "s/{{RESOURCE_ID}}/$INSTANCE_ID/g; s/{{INSTANCE_TYPE}}/$INSTANCE_TYPE/g; s/{{PRIVATE_IP}}/$PRIVATE_IP/g" "templates/instance_template.tf.j2" "aws_instance_$INSTANCE_ID.tf"
+    generate_tf "instance" "s|{{RESOURCE_ID}}|$INSTANCE_ID|g; s|{{INSTANCE_TYPE}}|$INSTANCE_TYPE|g; s|{{PRIVATE_IP}}|$PRIVATE_IP|g" "templates/instance_template.tf.j2" "aws_instance_$INSTANCE_ID.tf"
     import_resource "aws_instance.example" "$INSTANCE_ID"
 done <<< "$INSTANCES"
 
